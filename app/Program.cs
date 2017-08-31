24247de58d457ae33db0108772abf97854dc66e7
Jeremy Clark <jeremy@Jeremys-MacBook-Pro.local>
2017-08-31 09:11:40 -0700

    Original Project

---
+++ app/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+using Library;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine($"The answer is {new Thing().Get(19, 23)}");
+        }
+    }
+}
